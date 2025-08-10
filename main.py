b36383d7d1610a21dd72ee4e29d02f05c4e0a3ea
simon0302010 <simon0302010@gmail.com>
2025-08-10 09:57:29 -0400

    better support for multiple badges

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ cmd = [SX126X_CMD_GET_RSSI_INST]
 
 class App(badge.BaseApp):
     def __init__(self) -> None:
-        self.got_packet = False
-        self.packet = None
+        self.packets = []
+        self.packet_data = {}
 
     def on_open(self) -> None:
         badge.display.fill(1)
@@ -16,21 +16,20 @@ class App(badge.BaseApp):
         badge.display.show()
     
     def on_packet(self, packet: badge.radio.Packet, is_foreground: bool) -> None:
-        self.got_packet = True
-        self.packet = packet
+        self.packets.append([packet, sx.getRSSI()])
 
     def loop(self):
         # if badge.input.get_button(badge.input.Buttons.SW11):
-        badge.radio.send_packet(dest=0xFFFF, data="This is a test packet".encode())
-        # badge.buzzer.tone(600, 0.5)
+        badge.radio.send_packet(dest=0xFFFF, data="TBD".encode())
 
-        if self.got_packet:
-            badge.display.fill(1)
-            rssi = sx.getRSSI()
-            badge.buzzer.tone((-rssi if -rssi > 0 else 1) * 30, 0.5)
-            badge.display.text(str(rssi), 10, 10, 0)
-            badge.display.text(self.packet.data.decode(), 10, 50, 0)
-            self.got_packet = False
+        if len(self.packets):
+            self.packet_data = {}
+            for packet in self.packets:
+                self.packet_data[str(packet[0].source)] = [packet[1], packet[0].data.decode()]
+            badge.buzzer.tone(800, 0.2)           
+            badge.display.text(f"Got {str(len(self.packets))} packet(s)", 10, 10, 0)
+            print(self.packet_data)
             badge.display.show()
+            self.packets = []
 
         utime.sleep_ms(2500)
