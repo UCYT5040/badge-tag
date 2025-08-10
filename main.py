74791835c5117b5d59ad452c4b8de194321101bd
simon0302010 <simon0302010@gmail.com>
2025-08-10 01:11:48 -0400

    automatic sending of messages

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class App(badge.BaseApp):
 
     def on_open(self) -> None:
         badge.display.fill(1)
-        badge.display.text("Press SW11 to send", 10, 180, 0)
+        # badge.display.text("Press SW11 to send", 10, 180, 0)
         badge.display.show()
     
     def on_packet(self, packet: badge.radio.Packet, is_foreground: bool) -> None:
@@ -20,17 +20,17 @@ class App(badge.BaseApp):
         self.packet = packet
 
     def loop(self):
-        if badge.input.get_button(badge.input.Buttons.SW11):
-            badge.radio.send_packet(dest=0xFFFF, data="This is a test packet".encode())
-            badge.buzzer.tone(600, 0.5)
+        # if badge.input.get_button(badge.input.Buttons.SW11):
+        badge.radio.send_packet(dest=0xFFFF, data="This is a test packet".encode())
+        # badge.buzzer.tone(600, 0.5)
 
         if self.got_packet:
-            badge.buzzer.tone(2000, 0.5)
             badge.display.fill(1)
             rssi = sx.getRSSI()
+            badge.buzzer.tone((-rssi if -rssi > 0 else 1) * 30, 0.5)
             badge.display.text(str(rssi), 10, 10, 0)
             badge.display.text(self.packet.data.decode(), 10, 50, 0)
             self.got_packet = False
             badge.display.show()
 
-        utime.sleep_ms(50)
+        utime.sleep_ms(2500)
