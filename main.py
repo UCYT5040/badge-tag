298ed1158a6b355ca01f94fc24159b6be5be0310
simon0302010 <simon0302010@gmail.com>
2025-08-10 20:35:56 -0400

    further improvements

--- main.py
+++ main.py
@@ -8,6 +8,40 @@ except ImportError:
 
 from internal_os.hardware.radio import sx
 
+pacman_melody_simple = [
+    [494, 0.14285714285714285],   # B4, 16th
+    [988, 0.14285714285714285],   # B5, 16th
+    [740, 0.14285714285714285],   # F#5, 16th
+    [622, 0.14285714285714285],   # D#5, 16th
+    [988, 0.07142857142857142],   # B5, 32nd
+    [740, 0.21428571428571427],   # F#5, dotted 16th
+    [622, 0.2857142857142857],    # D#5, 8th
+    [523, 0.14285714285714285],   # C5, 16th
+    [1047, 0.14285714285714285],  # C6, 16th
+    [1568, 0.14285714285714285],  # G6, 16th
+    [1319, 0.14285714285714285],  # E6, 16th
+    [1047, 0.07142857142857142],  # C6, 32nd
+    [1568, 0.21428571428571427],  # G6, dotted 16th
+    [1319, 0.2857142857142857],   # E6, 8th
+    [494, 0.14285714285714285],   # B4, 16th
+    [988, 0.14285714285714285],   # B5, 16th
+    [740, 0.14285714285714285],   # F#5, 16th
+    [622, 0.14285714285714285],   # D#5, 16th
+    [988, 0.07142857142857142],   # B5, 32nd
+    [740, 0.21428571428571427],   # F#5, dotted 16th
+    [622, 0.2857142857142857],    # D#5, 8th
+    [622, 0.07142857142857142],   # D#5, 32nd
+    [659, 0.07142857142857142],   # E5, 32nd
+    [698, 0.07142857142857142],   # F5, 32nd
+    [698, 0.07142857142857142],   # F5, 32nd
+    [740, 0.07142857142857142],   # F#5, 32nd
+    [784, 0.07142857142857142],   # G5, 32nd
+    [784, 0.07142857142857142],   # G5, 32nd
+    [831, 0.07142857142857142],   # G#5, 32nd
+    [880, 0.14285714285714285],   # A5, 16th
+    [988, 0.2857142857142857],    # B5, 8th
+]
+
 class App(badge.BaseApp):
     def __init__(self) -> None:
         self.packets = []
@@ -62,6 +96,9 @@ class App(badge.BaseApp):
                             badge.radio.send_packet(dest=0xFFFF, data=str(sx.getRSSI()).encode())
                             if -sx.getRSSI() < 10:
                                 self.role = "S"
+                                for tone in pacman_melody_simple:
+                                    badge.buzzer.tone(tone[0], tone[1] * 0.9)
+                                    utime.sleep(tone[1] * 0.1)
 
 
                 badge.display.text(f"Found {str(len(self.packets))} player(s)", 10, 180, 0)
@@ -76,10 +113,12 @@ class App(badge.BaseApp):
             if badge.input.get_button(badge.input.Buttons.SW13):
                 self.role = "H"
                 badge.display.fill(1)
+                badge.display.text("You are a hider", 10, 10, 0)
                 badge.display.show()
             elif badge.input.get_button(badge.input.Buttons.SW6):
                 self.role = "S"
                 badge.display.fill(1)
+                badge.display.text("You are a seeker", 10, 10, 0)
                 badge.display.show()
             utime.sleep_ms(50)
 
